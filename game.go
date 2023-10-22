bc11f56e6718add100beffb520f8e9ec8d628fac
ilia <tsuprik.ilia@gmail.com>
2023-10-22 19:53:58 +0200

    Add generation label to the bottom

--- game.go
+++ game.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"image"
-	"image/color"
+	"strconv"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
 )
 
 type Game struct {
-	raster *canvas.Raster
-	field  *Field
+	raster          *canvas.Raster
+	field           *Field
+	container       *fyne.Container
+	generationLabel *widget.Label
 }
 
 func (game *Game) draw(w, h int) image.Image {
@@ -21,7 +25,7 @@ func (game *Game) draw(w, h int) image.Image {
 	for i := 0; i < FIELD_LENGTH; i++ {
 		for j := 0; j < FIELD_HEIGHT; j++ {
 			if game.field.cells[i][j] {
-				rasterImg.Set(i, j, color.Black)
+				rasterImg.Set(i, j, CELL_COLOR)
 			}
 		}
 	}
@@ -32,21 +36,29 @@ func (game *Game) draw(w, h int) image.Image {
 func (game *Game) runGameLoop() {
 	for range time.NewTicker(time.Second / time.Duration(SPEED)).C {
 		game.raster.Refresh()
-		fmt.Println("Generation: ", game.field.generation)
+		game.generationLabel.SetText(strconv.Itoa(game.field.generation))
 
 		game.field.calculateNextState()
 	}
 }
 
-func runGame(gameWindow fyne.Window) {
+func createGame() Game {
 	game := Game{}
 	game.raster = canvas.NewRaster(game.draw)
 	game.raster.ScaleMode = canvas.ImageScalePixels
+	game.raster.SetMinSize(fyne.NewSize(float32(SCREEN_WIDTH_PX), float32(SCREEN_HEIGHT_PX)))
 
 	game.field = createNewField()
 	game.field.setRandomFieldState()
 
-	gameWindow.SetContent(game.raster)
+	game.generationLabel = widget.NewLabel("0")
+	labelBox := container.New(
+		layout.NewHBoxLayout(),
+		layout.NewSpacer(),
+		widget.NewLabel("Generation: "),
+		game.generationLabel,
+	)
+	game.container = container.New(layout.NewVBoxLayout(), game.raster, labelBox)
 
-	go game.runGameLoop()
+	return game
 }
--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
 var (
-	FIELD_LENGTH = 130
-	FIELD_HEIGHT = 100
-	SPEED        = 8
+	FIELD_LENGTH     = 130
+	FIELD_HEIGHT     = 100
+	SPEED            = 8
+	SCREEN_WIDTH_PX  = 1000
+	SCREEN_HEIGHT_PX = 700
+	CELL_COLOR       = color.RGBA{R: 55, G: 102, B: 68, A: 255}
 )
 
 func main() {
-	// TODO:
-	// 1. Add a label with generation #
-	// 2. Change cell colors
-	// 3*. Add start button
 	gameApp := app.New()
 	gameWindow := gameApp.NewWindow("Game Life")
 
-	runGame(gameWindow)
+	game := createGame()
+	gameWindow.SetContent(game.container)
+	gameWindow.Resize(fyne.NewSize(float32(SCREEN_WIDTH_PX), float32(SCREEN_HEIGHT_PX)))
+	go game.runGameLoop()
 
-	gameWindow.Resize(fyne.NewSize(1000, 700))
 	gameWindow.ShowAndRun()
 }
